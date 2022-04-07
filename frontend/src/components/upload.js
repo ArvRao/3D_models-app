42c07dfd2f92a21e9c9c12ddb4c7f1ae738ec155
arvrao <arvindrao100@gmail.com>
2022-04-07 08:37:34 +0530

    backend upload update

--- frontend/src/components/upload.js
+++ frontend/src/components/upload.js
@@ -5,44 +5,44 @@ import axios from 'axios';
 
 const Upload = () => {
 
-    const [file, setFile] = useState()
+    const [file, setFile] = useState({ preview: '', data: '' })
+    const [status, setStatus] = useState('')
 
-    const uploadModel = e => {
-        let files = e.target.files
-        console.warn(files)
-        setFile(e.target.files[0])
-
-    }
-
-    const onSubmit = (e) => {
+   
+    const handleSubmit = async (e) => {
         e.preventDefault()
-
-        const data = new FormData()
-        data.append('file', file)
-
-        axios.post('localhost:5000/upload', data)
-            .then((e) => {
-                console.log('Success')
-            })
-            .catch((e) => {
-                console.log('Error', e)
-            })
-
+        let formData = new FormData()
+        formData.append('file', file.data)
+        const response = await fetch('http://localhost:5000/file', {
+            method: 'POST',
+            body: formData,
+        })
+        if (response) setStatus(response.statusText)
     }
 
+    const handleFileChange = (e) => {
+        const img = {
+            preview: URL.createObjectURL(e.target.files[0]),
+            data: e.target.files[0],
+        }
+        setFile(img)
+    }
 
+   
 
     return (
         <div className="pt-6">
             <h1 className="text-lg font-semibold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 font-serif">Upload a model</h1> <br />
             {/* <button className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-full w-36 border-2 py-2" onClick={uploadModel}>Click here</button> */}
-            <form onSubmit={onSubmit} method="post">
-            <input type="file" name="file" onChange={uploadModel} />
-            <button>Submit</button>
-            
-            </form>
+            <hr />
+            <form onSubmit={handleSubmit}>
+                <input type='file' name='file' onChange={handleFileChange}></input>
+                <button className="bg-blue-500" type='submit'>Submit</button>
+    
+</form>
+            {status && <h4>{status}</h4>}
 
-            </div>
+        </div>
     )
 }
 
